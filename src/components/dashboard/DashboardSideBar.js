c092db576f0cc406acbba9e4c2a5a00e4c3b07e9
Kyle C <kyclutter@gmail.com>
2020-12-11 13:43:21 -0600

    updates

--- src/components/dashboard/DashboardSideBar.js
+++ src/components/dashboard/DashboardSideBar.js
@@ -16,7 +16,6 @@ const DashboardSideBar = () => {
             <div className="dsidebar-2">
                 
                     <div className="dsidebar-2-menu-item">
-<<<<<<< HEAD
                            <div className="">Icon</div>
                            <div className="">Open Orders</div>
                     </div>
@@ -30,21 +29,6 @@ const DashboardSideBar = () => {
                     </div>
                     <div className="dsidebar-2-menu-item">
                            <div className="">Icon</div>
-=======
-                           <ImportContactsIcon/>
-                           <div className="">Open Orders</div>
-                    </div>
-                    <div className="dsidebar-2-menu-item">
-                           <ListIcon/>
-                           <div className="">Watch List </div>
-                    </div>
-                    <div className="dsidebar-2-menu-item">
-                           <LocalConvenienceStoreIcon/>
-                           <div className="">Markets</div>
-                    </div>
-                    <div className="dsidebar-2-menu-item">
-                           <ImportExportIcon/>
->>>>>>> d2a5c2a33db0c0cde0d111bc224e71daf09875c9
                            <div className="">Gains / Losses</div>
                     </div>
                     
